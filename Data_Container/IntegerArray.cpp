c196d00e0cdc0ba64c3333eda4223165f526eec9
AzadHesso <azad.hesso90@gmail.com>
2022-11-10 09:00:59 +0300

    Exception bad_range and overload operator []

--- Data_Container/IntegerArray.cpp
+++ Data_Container/IntegerArray.cpp
@@ -1,17 +1,8 @@
 #include "IntegerArray.h"
 #include <iostream>
-//#include <cassert> // for assert()
-
-const char* Bad_length::what() const noexcept
-{
-
-    return "ERROR: Index is out of Range!";
-
-}
 
 IntegerArray::IntegerArray(int length) : m_length(length)
 {
-    //assert(length >= 0);
     if (length <= 0)
     {
         throw Bad_length();
@@ -20,9 +11,45 @@ IntegerArray::IntegerArray(int length) : m_length(length)
         m_data = new int[length] {};
 }
 
+IntegerArray::~IntegerArray()
+{
+    delete[] m_data;
+}
+
+//void integerarray::erase()
+//{
+//    delete[] m_data;
+//    m_data = nullptr;
+//    m_length = 0;
+//}
+
+int& IntegerArray::operator[](int index)
+{
+    if (index <= 0 && index > m_length)
+    {
+        throw Bad_range();
+    }
+    return m_data[index];
+}
+
+int IntegerArray::getLength() const
+{
+    return m_length;
+}
+
 void IntegerArray::show()
     {
         std::cout << m_length << std::endl;
     }
 
+const char* Bad_length::what() const noexcept
+{
 
+    return "ERROR: giving the wrong length to an array!";
+
+}
+
+const char* Bad_range::what() const noexcept
+{
+    return "Error: length out of range!";
+}
--- Data_Container/IntegerArray.h
+++ Data_Container/IntegerArray.h
@@ -16,15 +16,6 @@
 
 using namespace std;
 
-class Bad_length : public exception
-{
-public:
-    Bad_length() = default;
-   ~Bad_length() = default;
-
-    virtual const char* what() const noexcept override;
-};
-
 class IntegerArray
 {
 private:
@@ -35,8 +26,34 @@ public:
     IntegerArray() = default;
 
     IntegerArray(int length);
+
+    ~IntegerArray();
+
+  // void erase();
+
+    int& operator[](int index); //перегрузим оператор [], чтобы мы могли получить доступ к элементам массива. 
+                                //ћы должны проверить индекс, чтобы убедитьс€, что он действителен
+
+    int getLength() const;      //функцию доступа дл€ возврата длины массива
    
     void show();
   
+};
 
+class Bad_length : public exception
+{
+public:
+    Bad_length() = default;
+   ~Bad_length() = default;
+
+    virtual const char* what() const noexcept override;
+};
+
+class Bad_range : public exception
+{
+public:
+    Bad_range() = default;
+   ~Bad_range() = default;
+
+   virtual const char* what() const noexcept override;
 };
